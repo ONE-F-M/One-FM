7ef30085874d3fc4d67ad4f11610f6d511ece202
osamaasim <frappe@ubuntu.vm>
2021-03-13 21:12:38 +0000

    Checking that dev_osama works

--- one_fm/fleet_management/doctype/vehicle_leasing_contract/vehicle_leasing_contract.py
+++ one_fm/fleet_management/doctype/vehicle_leasing_contract/vehicle_leasing_contract.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2020, omar jaber and contributors
 # For license information, please see license.txt
-#
+# Checking again
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
